94d658334eed982f98866128d8782c1f8b5161b4
Atikan Chinwanno <degun2548@gmail.com>
2026-08-30 18:18:25 +0700

    wip: stage 3 scheduler + rabbitmq

---
+++ scheduler/scheduler.py
@@ -0,0 +1,31 @@
+import os, time, pika
+from bson import json_util
+from producer import produce
+from database import get_router_info
+
+RABBITMQ_HOST = os.environ.get("RABBITMQ_HOST", "localhost")
+
+def scheduler():
+    INTERVAL = 10.0
+    next_run = time.monotonic()
+    count = 0
+
+    while True:
+        now = time.time()
+        now_str = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now))
+        ms = int((now % 1) * 1000)
+        print(f"[{now_str}.{ms:03d}] run #{count}")
+
+        try:
+            for data in get_router_info():
+                body_bytes = json_util.dumps(data).encode("utf-8")
+                produce(RABBITMQ_HOST, body_bytes)
+        except Exception as e:
+            print(e)
+            time.sleep(3)
+        count += 1
+        next_run += INTERVAL
+        time.sleep(max(0.0, next_run - time.monotonic()))
+
+if __name__ == "__main__":
+    scheduler()
